ef2a96fd961e78812b2f8260a46350404d920651
MasterX01 <aakashsaxena14@gmail.com>
2021-03-30 15:17:42 +0530

    [Akash] FIRST COMMIT

---
+++ src/main/java/com/bridgelabz/stackandqueue/MyStackAndQueue.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.stackandqueue;
+
+public class MyStackAndQueue {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Stack And Queue Program");
+    }
+}
